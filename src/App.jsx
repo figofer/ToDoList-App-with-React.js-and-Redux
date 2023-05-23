8fb71327ffe1c716a0935293d2ba1a28a52e8246
figofer <figoferdian18@gmail.com>
2023-05-24 02:01:40 +0700

    intalling vite, react, redux, react-icon

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import MainLayout from "./layouts/MainLayout";
+import ContentLayout from "./layouts/ContentLayout";
+import InputTodo from "./modules/InputTodo";
+import ListTodo from "./modules/ListTodo";
+import { useState } from "react";
+
+const App = () => {
+  const [id, setId] = useState(0);
+  const [todo, setTodo] = useState("");
+  const [isUpdate, setIsUpdate] = useState(false);
+
+  const getTodo = (id, todo) => {
+    setId(id);
+    setTodo(todo);
+    setIsUpdate(true);
+  };
+
+  const updateDone = () => setIsUpdate(false);
+
+  return (
+    <MainLayout>
+      <ContentLayout>
+        <h1 className="">
+          What's the plan for today ?
+        </h1>
+        <section className="">
+          <InputTodo
+            id={id}
+            todoUpdate={todo}
+            isUpdate={isUpdate}
+            done={updateDone}
+          />
+
+          <ListTodo getTodo={getTodo} />
+        </section>
+      </ContentLayout>
+    </MainLayout>
+  );
+};
+
+export default App;
